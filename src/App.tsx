958d1a65720e328f0d840374a70406359cd76226
Lukas Klingberg <Lukas@Klingberg.se>
2021-04-12 19:59:57 +0200

    non working app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
+import { render } from '@testing-library/react';
+import React,{useState} from 'react';
 import './App.css';
 
+
+
 function App() {
+
+const [task, setTask] = useState("")
+const [list, setList] = useState([]);
+
+function addToList(e:any){
+  
+  setTask(e)
+  
+}
+
+function add(){
+  setList(task)
+  
+}
+
+function renderListItems(){
+  return list.map((newItem)=> <li>{newItem}</li>)
+}
+
+
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+      <div>
+      <h2>My To Do List</h2>
+      <input type = "text" onChange = {e=>addToList(e.target.value)}>
+      </input>
+
+      <button onClick = {add}> Add
+      </button>
+
+      <ul>{listItems}</ul>
+      
+      </div>
+
+     
+
+     
+  
       </header>
     </div>
   );
-}
 
+  }
 export default App;
+  
